994d7c59ef8e08942cfb8fe5cdbf1affd45f2fba
muhammedfawzii <mohammed.fawzzyy@gmail.com>
2024-10-08 16:58:54 -0700

    new

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   board!:string[]
   playerTurn:string = 'X'
+  gameEnd:boolean = false
   winner = [
     [0 , 1 , 2],
     [3 , 4 , 5],
@@ -23,13 +24,33 @@ export class HomeComponent implements OnInit {
 ngOnInit(): void {
   this.board = new Array(9).fill('')
 }
-player(index:number){
-this.board[index] = this.playerTurn
-this.playerTurn = this.playerTurn == 'X'? 'O': 'X'
+// player(index:number){
+// this.board[index] = this.playerTurn
+// this.playerTurn = this.playerTurn == 'X'? 'O': 'X'
+// this.gameEnd = true
+// }
+player(index: number) {
+  if (!this.board[index] && !this.gameEnd) { // Check if square is empty and game hasn't ended
+    this.board[index] = this.playerTurn;
+    if (this.checkWinner()) { // Check if the current player has won
+      this.gameEnd = true; // Set gameEnded to true
+    } else {
+      this.playerTurn = this.playerTurn === 'X' ? 'O' : 'X'; // Switch turns
+    }
+  }
 }
 resetGame():void{
 this.board = new Array(9).fill('')
 this.playerTurn = 'X'
+this.gameEnd = false
 
+}
+checkWinner(): boolean {
+  // Use 'some' to check for a winner
+  return this.winner.some(combin => 
+    this.board[combin[0]] && 
+    this.board[combin[0]] === this.board[combin[1]] && 
+    this.board[combin[1]] === this.board[combin[2]]
+  );
 }
 }
